27070f683c67e073e6c4b6e4317e6ba1bfeafc44
Eduardo Gomes Heleno <eduardo.gheleno@gmail.com>
2021-09-12 19:57:20 -0300

    :wrench: Environment variables to deploy

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ const config = require('./config');
 const app = express();
 const router = express.Router();
 
-mongoose.connect(config.connectionString, {
+mongoose.connect(process.env.DATABASE_URL, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
